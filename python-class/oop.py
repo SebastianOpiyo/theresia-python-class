3c27744b70c2818a53c992df67ed55cfdfec7647
doka <sebamopale@gmail.com>
2026-08-02 20:09:31 +0300

    Class: Inheritance concepts.

--- python-class/oop.py
+++ python-class/oop.py
@@ -13,13 +13,15 @@ class Vehicle:
           self.color = color
           self.num_tires = number_tires
           self.engine = engine
+          self.fuel_type = None
 
      def drive(self):
           print(f"Driving")
     
-     def fuel(self):
-          print(f"fueling")
-   
+     def fuel(self, fuel_type:str):
+          self.fuel_type = fuel_type
+          return self.fuel_type
+          
 
 # instantiate
 Car = Vehicle("red",4,"2000cc")  # An instance of a vehicle in the name of a car
@@ -28,7 +30,7 @@ Lorry = Vehicle("white",8,"3500cc")
 
 # DAY 2 ON CLASSES AND OBJECTS
 
-class Car:
+class Car(Vehicle):
 
      location_of_manufacture = "Japan" # An example of a class attribute
 
@@ -38,8 +40,8 @@ class Car:
           self.make = make
 
      def define(self):
-          print(f"The car is a {self.color}, manufactured in the year {self.year} and its make is {self.make}")
-          return
+          # print(f"The car is a {self.color}, manufactured in the year {self.year} and its make is {self.make} and its fuel is {self.fuel("Diesel")}")
+          print(f"The Company name is {self.company_name}\nAnd the location of manufacturing is {self.location_of_manufacture}")
 
 
 
@@ -79,8 +81,8 @@ suzuki_car = Car("blue", 2021, "Suzuki")
 
 
 if __name__ == "__main__":
-    print(toyota_car.location_of_manufacture) 
-    print(suzuki_car.location_of_manufacture) 
+    print(toyota_car.define()) 
+#   print(suzuki_car.location_of_manufacture) 
     # This will raise an error because car_number is not defined in this scope
 
 
